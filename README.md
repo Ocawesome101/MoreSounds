c2ef30dc22421fb4556dfc186f3335d42f936b6d
Ocawesome101 <ocawesome101@gmail.com>
2022-08-16 15:28:59 -0400

    fixed it!! and added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# More Sounds
+
+This is a mod for Minecraft Forge 1.18.2 that completely removes the rather arbitrary maximum polyphony limit.  It may work on other versions.
+
+You will need to use your system installation of OpenAL\* (easiest in [PolyMC](https://github.com/PolyMC/PolyMC)) and configure it to have the number of channels you need--I recommend 1024.  On Linux you can do this by running `alsoft-config` in a terminal if the necessary libraries are installed.
+
+\* *There may be a way to configure Minecraft's OpenAL installation.  I am unaware of it.*
--- src/main/java/com/ocawesome/mixins/MixinCountingChannelPool.java
+++ src/main/java/com/ocawesome/mixins/MixinCountingChannelPool.java
@@ -4,12 +4,14 @@ import com.mojang.blaze3d.audio.Channel;
 import com.mojang.blaze3d.audio.Library;
 import com.mojang.logging.LogUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
+import java.lang.annotation.Target;
 import java.util.Set;
 
 @Mixin(targets = "com.mojang.blaze3d.audio.Library$CountingChannelPool")
@@ -17,15 +19,14 @@ abstract class MixinCountingChannelPool implements Library.ChannelPool {
     @Shadow
     private Set<Channel> activeChannels;
 
-    @Inject(at = @At("HEAD"), method = "acquire()Lcom/mojang/blaze3d/audio/Channel;")
+    @Inject(at = @At("HEAD"), method = "acquire()Lcom/mojang/blaze3d/audio/Channel;", cancellable = true)
     private void acquire(CallbackInfoReturnable<Channel> callback) {
         @Nullable
         Channel channel = Channel.create();
-        Library.CountingChannelPool self = (Library.CountingChannelPool) (Object) this;
+        //Library.CountingChannelPool self = (Library.CountingChannelPool) (Object) this;
         if (channel != null) {
-            self.activeChannels.add(channel);
+            this.activeChannels.add(channel);
         }
-        LogUtils.getLogger().info("ALLOCATED CHANNEL");
         callback.setReturnValue(channel);
         callback.cancel();
     }
